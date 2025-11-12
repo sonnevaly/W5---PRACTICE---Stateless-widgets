2b056e135fac2aceb7a5397a2dfefd4a8439a177
sonnevaly <valygamer999@gmail.com>
2025-11-12 23:23:01 +0700

    done1

--- lib/week5/ex1.dart
+++ lib/week5/ex1.dart
@@ -3,58 +3,84 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-class HobbyCard extends StatelessWidget{
-  final String title;
-  final IconData icon;
-  final Color? color;
-  const HobbyCard({super.key, required this.icon, required this.title,this.color = Colors.blue});
-  @override
-  Widget build(BuildContext context) {
-    return
-    Container(
-                  height: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20), 
-                    color: color,  ),
-                  child: Center(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Padding(padding: EdgeInsetsGeometry.all(30),),
-                        Padding(padding: EdgeInsetsGeometry.all(20),
-                          child: Icon(
-                          icon,
-                          color: Colors.white,
-                          ),
-                        ),
-                          Text(title ,style: TextStyle(fontSize: 32,color: Colors.white),),
-                      ],
-                    ),
-                  ),
-                );
-  }
-}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:
-        Scaffold(
-          appBar: AppBar(title: Text('My hobbies'),),
-          body: Padding(
-            padding: EdgeInsetsGeometry.all(50),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              spacing: 30,
-              children: [
-                HobbyCard(icon: Icons.travel_explore, title: 'Travelling',color: Colors.green,),
-                HobbyCard(title: 'Skating', icon: Icons.ice_skating,color: Colors.blueGrey,),
-              ],
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My hobbies'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(50), // fixed EdgeInsets
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: const [
+              HobbyCard(
+                icon: Icons.travel_explore,
+                title: 'Travelling',
+                color: Colors.green,
+              ),
+              SizedBox(height: 30), 
+              HobbyCard(
+                title: 'Skating',
+                icon: Icons.ice_skating,
+                color: Colors.blueGrey,
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
+  }
+}
+
+class HobbyCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Color? color;
+
+  const HobbyCard({
+    super.key,
+    required this.icon,
+    required this.title,
+    this.color = Colors.blue, 
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: color,
+      ),
+      child: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(width: 30), 
+            const SizedBox(width: 10),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Icon(
+                icon,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 32,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
